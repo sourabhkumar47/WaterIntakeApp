41a0ebb16c477204abd09ebf7fd0eea80562d3c0
Sourabh Kumar <20010001030@gateway.edu.in>
2022-10-02 01:08:04 +0530

    Added round corner to button

--- app/src/main/java/com/example/waterintakeapp_state/WaterCounter.kt
+++ app/src/main/java/com/example/waterintakeapp_state/WaterCounter.kt
@@ -2,6 +2,7 @@ package com.example.waterintakeapp_state
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
@@ -17,7 +18,8 @@ fun WaterCounter(modifier: Modifier = Modifier) {
         Text("You've had ${count} glasses.")
         Button(
             onClick = { count++ },
-            Modifier.padding(top = 8.dp)
+            Modifier.padding(top = 8.dp),
+            shape = RoundedCornerShape(24.dp)
         ) {
             Text("Add one")
         }
