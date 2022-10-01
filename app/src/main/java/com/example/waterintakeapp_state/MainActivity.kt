e9f2824b47620a6fc1721847e6a296db2b36327a
Sourabh Kumar <20010001030@gateway.edu.in>
2022-10-01 23:56:43 +0530

    Added Button

--- app/src/main/java/com/example/waterintakeapp_state/MainActivity.kt
+++ app/src/main/java/com/example/waterintakeapp_state/MainActivity.kt
@@ -29,11 +29,6 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
