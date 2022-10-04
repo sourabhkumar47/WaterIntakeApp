d84acc3fde24930912c9a49a86794adb31c730cf
Sourabh Kumar <20010001030@gateway.edu.in>
2022-10-05 00:15:27 +0530

    Added state driven UI

--- app/src/main/java/com/example/waterintakeapp_state/WaterCounter.kt
+++ app/src/main/java/com/example/waterintakeapp_state/WaterCounter.kt
@@ -24,6 +24,7 @@ fun WaterCounter(modifier: Modifier = Modifier) {
         Button(
             onClick = { count++ },
             Modifier.padding(top = 8.dp),
+            enabled = count < 10,
             shape = RoundedCornerShape(24.dp)
         ) {
             Text("Add one")
