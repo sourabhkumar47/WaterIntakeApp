3530881b10e8b012ee43ddb943b158b63753efa2
Sourabh Kumar <20010001030@gateway.edu.in>
2022-10-06 22:03:40 +0530

    Added clean button

--- app/src/main/java/com/example/waterintakeapp_state/WaterCounter.kt
+++ app/src/main/java/com/example/waterintakeapp_state/WaterCounter.kt
@@ -1,6 +1,7 @@
 package com.example.waterintakeapp_state
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
@@ -20,7 +21,7 @@ fun WaterCounter(modifier: Modifier = Modifier) {
             if (showtask) {
                 WellnessTaskItem(
                     taskName = "Have you taken 15 min walk today?",
-                    onClose = {showtask = false }
+                    onClose = { showtask = false }
                 )
             }
             // This text is present if the button has been clicked
@@ -28,14 +29,23 @@ fun WaterCounter(modifier: Modifier = Modifier) {
             Text("You've had ${count} glasses.")
         }
 
-        Button(
-            onClick = { count++ },
-//            Modifier.padding(top = 8.dp),
-            enabled = count < 10,
-            shape = RoundedCornerShape(24.dp)
-        ) {
-            Text("Add one")
+        Row(Modifier.padding(top = 16.dp)) {
+            Button(
+                onClick = { count++ },
+                enabled = count < 10,
+                shape = RoundedCornerShape(24.dp)
+            ) {
+                Text("Add one")
+            }
+            Button(
+                onClick = { count = 0 },
+                Modifier.padding(start = 16.dp),
+                shape = RoundedCornerShape(24.dp)
+            ) {
+                Text("Clean water count")
+            }
         }
+
     }
 
 }
